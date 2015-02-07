56b8349be1fe7a5e618b570b20f0614f377fc980
Edgardo Angel <edgardoangel@gmail.com>
2015-02-06 18:03:12 -0600

    new version

--- main.c
+++ main.c
@@ -9,15 +9,14 @@
  ./a.out -b # -s #
 */
 
+
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
-#define KB *1024
+#include <unistd.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char ** argv) 
 {
-
 	int b, s, c;
 	char *bchar, *schar;
 	extern char *optarg;
@@ -36,27 +35,49 @@ int main(int argc, char *argv[])
 		
 	printf("Block size: %d \n",b);
 	printf("Memory Size %d \n",s);
-
- /*
-	int M = 1024;
-	int b = 128;
+	
+    
+    /*
+    int b = 128;
+	int M = b * 11;  // so we have space for 11 items
+	
+	char buf [1024];
+	memset (buf, 1, 1024);		// set each byte to 1
+	
+	char * msg = "a sample message";
+	
 	Init (M,b); // initialize
 	// test operations
 	int testnums [] = {100, 5, 200, 7, 39, 25, 400, 50, 200, 300};
 	int i = 0;
+	// some sample insertions
 	for (i=0; i< 10; i ++)
 	{
-		Insert (testnums [i]);
+		Insert (testnums [i], buf, 50);   // insert 50 bytes from the buffer as value for each of the insertions
 	}
+	Insert (150, buf, 200); // this Insert should fail
 	PrintList ();
 	Delete (7);
-	Insert (13);
+	Insert (13, msg, strlen(msg)+1);		// insertion of strings, copies the null byte at the end
 	Delete (55);
+	Insert (15, "test msg", 8);
 	Delete (3);
 	PrintList ();
+	
+	// a sample lookup operations that should return null, because it is looking up a non-existent number
+	char* kv = Lookup (3);
+	if (kv)
+		printf ("Key = %d, Value Len = %d\n", *(int *) kv, *(int *) (kv+4));
+	
+	
+	// this look up  should succeed and print the string "a sample message"
+	kv = Lookup (13);
+	if (kv)
+		printf ("Key = %d, Value Len = %d, Value = %s\n", *(int *) kv, *(int *) (kv+4), kv + 8);
+	
+	
 	// end test operations	
 	Destroy ();
-	*/
-
-	return 0;
+     */
+	
 }
